87a2bffc3c2ef45282b767b09824afc6c98754ca
jagritx@gmail.com <71010470+Jagrit159@users.noreply.github.com>
2020-11-18 17:38:03 +0530

    asyncballmovement

--- sketch.js
+++ sketch.js
@@ -1,7 +1,10 @@
 var ball;
-
+var database, position, ballPosition;
 function setup(){
     createCanvas(500,500);
+    database = firebase.database();
+    ballPosition = database.ref("ball/position");
+    ballPosition.on("value",readPosition,showError);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
 }
@@ -9,21 +12,32 @@ function setup(){
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    database.ref("ball/position").set({
+        x : ball.x + x,
+        y : ball.y + y
+    })
+    
+}
+function readPosition(data){
+    position = data.val()
+    ball.x = position.x
+    ball.y = position.y
+}
+function showError(){
+    console.log("error");   
 }
